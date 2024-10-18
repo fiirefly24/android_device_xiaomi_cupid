34d6e41f31a87c8be60e614043f49c3b9e2be4dd
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    cupid: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_cupid.mk
+++ lineage_cupid.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,14 +16,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2201123G
 
-PRODUCT_SYSTEM_NAME := cupid_global
-PRODUCT_SYSTEM_DEVICE := cupid
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cupid_global-user 14 UKQ1.230917.001 V816.0.6.0.ULCMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := Xiaomi/cupid_global/cupid:14/UKQ1.230917.001/V816.0.6.0.ULCMIXM:user/release-keys
+    BuildDesc="cupid_global-user 14 UKQ1.230917.001 V816.0.6.0.ULCMIXM release-keys" \
+    BuildFingerprint=Xiaomi/cupid_global/cupid:14/UKQ1.230917.001/V816.0.6.0.ULCMIXM:user/release-keys \
+    DeviceProduct=cupid \
+    SystemName=cupid_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
