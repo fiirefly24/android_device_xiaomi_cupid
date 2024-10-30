19a0d65502f61491143e525e806bdc3c76e2cd4e
fiirefly24 <muhammadalifikri2412@gmail.com>
2024-10-30 14:11:29 +0000

    cupid: Bringup Pixelage

--- pixelage_cupid.mk
+++ pixelage_cupid.mk
@@ -8,18 +8,27 @@
 $(call inherit-product, device/xiaomi/cupid/device.mk)
 
 # Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_cupid
+# Signing Pixelage
+$(call inherit-product, vendor/pixelage-priv/keys/keys.mk)
+
+PRODUCT_NAME := pixelage_cupid
 PRODUCT_DEVICE := cupid
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2201123G
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
+BUILD_FINGERPRINT := Xiaomi/cupid_global/cupid:14/UKQ1.230917.001/V816.0.10.0.ULCMIXM:user/release-keys
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="cupid_global-user 14 UKQ1.230917.001 V816.0.10.0.ULCMIXM release-keys" \
-    BuildFingerprint=Xiaomi/cupid_global/cupid:14/UKQ1.230917.001/V816.0.10.0.ULCMIXM:user/release-keys \
+    BuildFingerprint=$(BUILD_FINGERPRINT) \
     DeviceProduct=cupid \
     SystemName=cupid_global
+
+PRODUCT_SYSTEM_PROPERTIES += ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
